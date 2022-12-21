47836e0e832aec8651a912465f8100182d59534c
magda19s <ms20050112@gmail.com>
2022-12-21 21:50:47 +0100

    Lista 11

---
+++ Lab11/Lab10v2/Controllers/ShopCartsController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Lab10v2.Data;
+using Lab10v2.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Lab10v2.Controllers
+{
+    public class ShopCartsController : Controller
+    {
+        private readonly MyDbContext _context;
+        CookieOptions options;
+        Dictionary<int, Article> _articles; 
+
+
+        public ShopCartsController(MyDbContext context)
+        {
+            _context = context;
+            _articles = new Dictionary<int, Article>();
+            options = new CookieOptions();
+        }
+
+        // GET: ShopCarts
+        public async Task<IActionResult> Index()
+        {
+            var cookies = Request.Cookies.Keys;
+            if (cookies.Count == 0)
+            {
+                return View();
+            }
+
+            var products = new List<ShopCart>();
+
+            foreach (var cookie in cookies)
+            {
+                List<Article> articles;
+                
+                int artId;
+
+                if (Int32.TryParse(cookie, out artId))
+                {
+                    articles = _context.Articles.Where(a => a.Id == artId).ToList();
+
+
+                    if (articles.Count() == 0)
+                    {
+                        TempData["Message"] = "Some products in your shopping cart are no longer available";
+                        Response.Cookies.Delete(cookie);
+                    }
+                    else
+                    {
+                        int amount = Int32.Parse(Request.Cookies[cookie]);
+                        ShopCart productsCart = new ShopCart()
+                        {
+                            Id = articles[0].Id,
+                            Name = articles[0].Name,
+                            TotalPrice = Math.Round(amount * articles[0].Price, 4),
+                            Category = _context.Categories.Where(a => a.Id == articles[0].CategoryId).FirstOrDefault(),
+                            Image = articles[0].FilePath,
+                            Amount = amount
+                        };
+                        products.Add(productsCart);
+                    }
+                }
+                
+            }
+
+
+            return View(products);
+        }
+
+        
+
+
+        public async Task<IActionResult> Increase(int id)
+        {
+            string myAmount = Request.Cookies[id.ToString()];
+            options.Expires = DateTime.Now.AddDays(7);
+            int value = Int32.Parse(myAmount) + 1;
+            Response.Cookies.Append(id.ToString(), value.ToString(), options);
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Decrease(int id)
+        {
+            string myAmount = Request.Cookies[id.ToString()];
+            int newAmount = Int32.Parse(myAmount) - 1;
+
+            if (newAmount == 0)
+            {
+                Response.Cookies.Delete(id.ToString());
+            }
+            else
+            {
+                options.Expires = DateTime.Now.AddDays(7);
+                Response.Cookies.Append(id.ToString(), newAmount.ToString(), options);
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            Response.Cookies.Delete(id.ToString());
+            return RedirectToAction("Index");
+
+        }
+
+        public IActionResult BuyArticle(int id)
+        {
+            options.Expires = DateTime.Now.AddDays(7);
+            string idOfIthem = id.ToString();
+            string myAmount = Request.Cookies[idOfIthem];
+            if (myAmount == null || myAmount == " ")
+            {
+                Response.Cookies.Append(id.ToString(), "1", options);
+            }
+            else
+            {
+                int value = Int32.Parse(myAmount) + 1;
+                Response.Cookies.Append(id.ToString(), value.ToString(), options);
+            }
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ Lab11/Lab10v2/Controllers/ShopController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Lab10v2.Models;
+using Lab10v2.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq;
+using System.IO;
+using System;
+
+namespace Lab10v2.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly MyDbContext _context;
+        private readonly IWebHostEnvironment _hostEnvironment;
+
+        public ShopController(MyDbContext context, IWebHostEnvironment hostEnvironment)
+        {
+            _context = context;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        //[Route("Shop/GetCategory/{categoryName}")]
+        public ActionResult Index(Result categoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                    categoryViewModel.Categories = _context.Categories.ToList().Select(x => new SelectListItem()
+                    {
+                        Text = x.Name,
+                        Value = x.Id.ToString()
+                    }).ToList();
+
+                    var articlesFromCategory = _context.Articles.ToList().FindAll(x => x.CategoryId == categoryViewModel.SelectedCategoryId);
+                    categoryViewModel.articles = articlesFromCategory;
+                
+            }
+            return View(categoryViewModel);
+        }
+
+        
+
+
+
+
+
+
+
+        // GET: ShopController
+        //public ActionResult Index()
+        //{
+        //    ViewData["CategoryName"] = new SelectList(_context.Categories.Select(c => c.Name), "Name");
+        //    return View("Views/Shop/Index.cshtml");
+        //}
+
+        //[Route("Shop/GetCategory/{categoryName}")]
+        //public IActionResult GetCategory(string categoryName)
+        //{
+        //    var articles = _context.Articles
+        //        .Where(a => a.Category.Name == categoryName)
+        //        .Select(a =>
+        //            new Article()
+        //            {
+        //                Id = a.Id,
+        //                Name = a.Name,
+        //                Price = a.Price,
+        //                CategoryId = a.CategoryId,
+        //                FilePath = a.FilePath,
+        //            }
+        //            )
+        //        .ToList();
+
+
+
+        //    return PartialView("Views/Shop/Table.cshtml", articles);
+        //}
+
+
+
+
+    }
+}
